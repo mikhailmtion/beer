8e29f4aa423a360fe7aea9ba11dc10ec0e7ce560
Mikhail <94560571+mikhailmtion@users.noreply.github.com>
2022-04-24 16:30:36 +0300

    update api url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'https://api.punkapi.com/v2/';
+const BASE_URL = 'https://api.punkapi.com/v2/beers';
 
-export const allBeers = (page) => BASE_URL + `beers?page=${page}&per_page=20`;
-export const searchById = (id) => BASE_URL + 'beers/' + id;
-export const RANDOM_BEER = BASE_URL + 'beers/random';
+export const allBeers = (page) => BASE_URL + `?page=${page}&per_page=20`;
+export const searchById = (id) => BASE_URL + '/' + id;
+export const RANDOM_BEER = BASE_URL + '/random';
